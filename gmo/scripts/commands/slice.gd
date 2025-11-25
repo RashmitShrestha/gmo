410fb4a46408ee4cb4be4d098dd74407c1b9d08a
RashmitShrestha <rashmit.stha@gmail.com>
2025-11-24 19:09:44 -0800

    updated slicing and added special attack trails

--- gmo/scripts/commands/slice.gd
+++ gmo/scripts/commands/slice.gd
@@ -1,18 +1,119 @@
+class_name Slice
 extends Line2D
-
 var points_queue : Array
-var max_points = 5 # limited trail
 
+var max_trail = 5
+var max_points = 100
 
-func slicing() -> void:
-	var pos = get_global_mouse_position()
+var current_element = -1
+var element_ready = -1
+
+var active_trails : Array = []
+
+var reg_colors={
+	0: Color(0.49, 0.82, .89, .6),
+	1: Color(1, 1, 1, .6)
+}
+var element_colors = {
+	1: Color(0.871, 0.235, 0.2, 0.7),
+	2: Color(0.275, 0.412, 0.988, 0.7),
+	3: Color(0.702, 0.937, 0.306, 0.7)
+}
+
+var element_duration = 10.0
+
+func _ready():
+	width = 20.0
+	set_as_top_level(true)
 	
-	points_queue.push_front(pos)
-	
-	if points_queue.size() > max_points:
-		points_queue.pop_back()
+func _process(delta):
+	for i in range(active_trails.size() - 1, -1, -1):
+		# reduce the active trails timer in process
+		active_trails[i].timer -= delta
 		
-	clear_points()
+		# if any are zero then queue the free and remove them 
+		if active_trails[i].timer <= 0:
+			active_trails[i].line.queue_free()
+			active_trails.remove_at(i)
+			
+			
+func slicing(mouse_pos: Vector2) -> void:
+	# push the points based on the global to local mouse position 
+	points_queue.push_front(to_local(mouse_pos))
 	
+	var max_size = 0
+	if current_element >= 0:
+		max_size = max_points
+	else:
+		max_size = max_trail
+	
+	
+	if points_queue.size() > max_size:
+		points_queue.pop_back()
+	
+	clear_points()
 	for p in points_queue:
 		add_point(p)
+		
+func start_trail(element: int) -> void:
+	if element in element_colors:
+		clear_points()
+		points_queue = []
+		
+		current_element = element
+		
+		# create a new gradient and color system based on the predefined element colors
+		var grad = Gradient.new()
+		var color = element_colors[element]
+		# the first stop at 0 is the darker version of the colors 
+		grad.add_point(0.0, Color(color.r - 0.3, color.g - 0.3, color.b - 0.3, 0.8))
+		grad.add_point(1.0, Color(color.r, color.g, color.b, 0.6))
+		
+		gradient = grad
+		width = 20.0
+		
+func end_trail() -> void:
+	# if the current element is not empty and the points queue is not empty
+	if current_element >= 0 and points_queue.size() > 0:
+		var trail_line = Line2D.new()
+		# important to set line as top level
+		trail_line.set_as_top_level(true)
+		trail_line.width = 10.0
+		
+		var grad = Gradient.new()
+		var color = element_colors[current_element]
+		grad.add_point(0.0, Color(color.r - 0.3, color.g - 0.3, color.b - 0.3, 0.8))
+		grad.add_point(1.0, Color(color.r, color.g, color.b, 0.6))
+		
+		trail_line.gradient = grad
+		
+		# turn local points to global points because Slice is inside Warden node
+		var global_points = []
+		for p in points_queue:
+			var global_point = to_global(p)
+			trail_line.add_point(global_point)
+			global_points.append(global_point)
+		
+		get_parent().add_child(trail_line)
+		
+		# active trails are based on the line, element, points, and timer
+		active_trails.append({
+			"line": trail_line,
+			"element": current_element,
+			"points": global_points,  # Store global points for collision detection
+			"timer": element_duration
+		})
+	
+	# changes the gradient and factors of the slice trail
+	var reg_grad = Gradient.new()
+	reg_grad.add_point(0, reg_colors[0])
+	reg_grad.add_point(1, reg_colors[1])
+	current_element = -1
+	element_ready = -1
+	gradient = reg_grad
+	width = 20
+	
+	
+# simply sets the ready element to the passed in element int 
+func set_element(element: int) -> void:
+	element_ready = element
--- gmo/scripts/components/character_components/player_components/player_input_component.gd
+++ gmo/scripts/components/character_components/player_components/player_input_component.gd
@@ -1,31 +1,46 @@
 class_name PlayerInputComponent
 extends InputComponent
 
-
 func update(event: InputEvent) -> void:
 	_parent.direction = Vector2(
 		int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")),
 		int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
 	).normalized()
 	
-	'''
+	var slice = _parent.get_node("Slice")
+	var mouse_pos = _parent.get_global_mouse_position()
+	
+	# pressing the number keys 1, 2, or 3 will set the element
+	# checks if current element is not a element
+	if Input.is_action_just_pressed("special_1") and slice.current_element < 0:
+		slice.set_element(1)
+		
+	elif Input.is_action_just_pressed("special_2") and slice.current_element < 0:
+		slice.set_element(2)
+		
+	elif Input.is_action_just_pressed("special_3") and slice.current_element < 0:
+		slice.set_element(3)
+	
+	# Handle slicing/drawing
 	if Input.is_action_pressed("left_click"):
 		if event is InputEventMouseMotion:
-			character.is_slicing = true
-			character.vel_vec = event.relative
-			character.curr_vel = abs(Vector2.ZERO.distance_to(character.vel_vec))
+			# If we have an element ready and haven't started slicing yet, activate it
+			if slice.element_ready >= 0 and slice.current_element < 0:
+				slice.start_trail(slice.element_ready)
 			
-			# tweak to find the best number for "full velocity hits" 
-			#if curr_vel > 25:
-				#print("F") # fast
-			#elif curr_vel > 7.5:
-				#print("M") # medium
-			#else: 
-				#print("S") # slow
+			# update the parents values as intended for slicing
+			_parent.is_slicing = true
+			_parent.vel_vec = event.relative
+			_parent.curr_vel = abs(Vector2.ZERO.distance_to(_parent.vel_vec))
 			
-			%Slice.slicing()
+			slice.slicing(mouse_pos)
 	else:
-		character.is_slicing = false
-		%Slice.clear_points()
-		%Slice.points_queue = []
-	'''
+		_parent.is_slicing = false
+		
+		# if we were using an element, end the slice
+		if slice.current_element >= 0:
+			slice.end_trail()
+		
+		# clear poitns when not slicing
+		slice.clear_points()
+		slice.points_queue = []
