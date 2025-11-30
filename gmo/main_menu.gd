fd000e530333c14214ec64a5b3228a0e659690f1
clairehbear <cehesley@ucdavis.edu>
2025-11-30 02:51:34 -0800

    quit button is functional

--- gmo/main_menu.gd
+++ gmo/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready():
 	%Play.pressed.connect(play)
+	%Quit.pressed.connect(quit)
 
 func play():
 	get_tree().change_scene_to_file("res://scenes/game_area.tscn")
+	
+func quit():
+	get_tree().quit()
