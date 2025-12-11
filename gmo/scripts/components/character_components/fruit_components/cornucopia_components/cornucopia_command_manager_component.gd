f04bd355585590a786261e9b9345abb6d58c337f
zkH2O <zizeng39@gmail.com>
2025-12-11 01:34:12 -0800

    fruit tracking + target switching fixed but the thing is some fruits get stuck on the tree

--- gmo/scripts/components/character_components/fruit_components/cornucopia_components/cornucopia_command_manager_component.gd
+++ gmo/scripts/components/character_components/fruit_components/cornucopia_components/cornucopia_command_manager_component.gd
@@ -9,7 +9,10 @@ func update():
 		if _just_attacked and not _just_retreated:
 			_just_attacked = false
 			_just_retreated = true
-			_parent.target = _parent.peach_tree
+			if _parent.peach_tree and not _parent.peach_tree.is_dead:
+				_parent.target = _parent.peach_tree
+			else:
+				_parent.target = _parent.warden
 			_parent.curr_command = _parent.retreat_command
 		elif not _just_attacked and _just_retreated:
 			_just_attacked = false
@@ -18,8 +21,11 @@ func update():
 		else:
 			_just_attacked = true
 			_just_retreated = false
-			
-			if randf() < 0.5:
+
+			if _parent.peach_tree and _parent.peach_tree.is_dead:
+				_parent.target = _parent.warden
+				_parent.curr_command = _parent.rush_command
+			elif randf() < 0.5:
 				_parent.target = _parent.warden
 				_parent.curr_command = _parent.rush_command
 			else:
